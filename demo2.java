7e672ce533364859af69af1f07b6cf086ba75e28
malikshamimhussain <malikshamim.13@gmail.com>
2020-08-20 14:28:34 +0200

    will check change in demo 2 modification on github

--- demo2.java
+++ demo2.java
@@ -3,7 +3,7 @@ public class demo2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
- System.out.println("new java class created");
+ System.out.println("new java class created and i want to check change");
 	}
 
 }
